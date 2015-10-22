b2f603547d91946b9fe46b87d26a8e51e200965c
Rich Brown <richb.hanover@gmail.com>
2015-10-22 00:31:17 -0400

    gulpfile.js now seems to work

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var autoprefixer = require('gulp-autoprefixer');
 
 
 gulp.task('styles',function() {
-  gulp.src('css/styles.css')
+  gulp.src('css/**/*.css')
     .pipe(autoprefixer())
     .pipe(gulp.dest('build'))
 });
 
 
 gulp.task('watch',function() {
-  gulp.watch('css/styles.css', ['styles']);
+  gulp.watch('css/**/*.css', ['styles']);
 });
